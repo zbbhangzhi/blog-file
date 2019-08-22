7fabd5ceadf96e768a129a3f8754e64d6c124f78
tiancha <tiancha@2dfire.com>
2019-08-22 18:59:17 +0800

    0822

--- li-zi/src/main/java/com/zbb/lizi/jdk/RateLimiter.java
+++
@@ -1,8 +0,0 @@
-package com.zbb.lizi.jdk;
-
-/**
- * @author by tiancha
- * @Date 2019/8/14 18:13
- */
-public class RateLimiter {
-}
---
+++ li-zi/src/main/java/com/zbb/lizi/jdk/concurrent/RateLimiter.java
@@ -0,0 +1,9 @@
+package com.zbb.lizi.jdk.concurrent;
+
+/**
+ * @author by tiancha
+ * @Date 2019/8/22 14:52
+ */
+public class RateLimiter {
+
+}
